add410e136ae2507198cbf65a8adae517a592722
aaronkh <aahuang@berkeley.edu>
2021-01-15 21:41:01 -0800

    add sockets boilerplate

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+const sockServer = require('http').Server(app)
+const io = require('socket.io')(sockServer, {origins: '*:*'})
 app.use(express.json())
 
 /* HTTP Methods 
@@ -21,5 +23,14 @@ app.use(express.json())
 /* Socket.io logic
  *
  */
+io.on('connection', sock => {
+    // Do something with the new socket
+    // socket.on('event', () => {
+    //      socket.emit('event name', {data: 1})
+    // })
+    return true
+})
+
+io.listen(process.env.WSPORT || 5000)
 
 app.listen(process.env.PORT || 8080, () => console.log('server started'))
