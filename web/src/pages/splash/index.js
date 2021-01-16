eecc5c771c4bdf93dd1c3f1797fa9bb73ddc9688
Krystal Chan <k1chan@ucsd.edu>
2021-01-16 00:01:47 -0800

    Splash

--- web/src/pages/splash/index.js
+++ web/src/pages/splash/index.js
@@ -1,5 +1,20 @@
+import './App.css'
+
 function Splash() {
-    return <></>
+    return <>
+    <div className = "smallTitle">
+        <h6>Coding in the Dark</h6>
+    </div>
+    <div className = "splash">
+        <h1 className = "header">Coding in the Dark</h1>
+
+        <p><input className = "input" type="text" id="name" name="name" value = "Name" required></input></p>
+        <p><input className = "input" type="text" id="codeid" name="name" value = "Code ID" required></input></p>
+        <button className = "button">Spectate</button>
+    </div>
+
+    </>
 }
+
 
 export default Splash
