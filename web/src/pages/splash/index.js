d62882f96e112276bde0cf2d4833660989965b65
Krystal Chan <k1chan@ucsd.edu>
2021-01-16 01:52:50 -0800

    Chat app

--- web/src/pages/splash/index.js
+++ web/src/pages/splash/index.js
@@ -5,6 +5,7 @@ function Splash() {
     <div className = "smallTitle">
         <h6>Coding in the Dark</h6>
     </div>
+    
     <div className = "splash">
         <h1 className = "header">Coding in the Dark</h1>
 
