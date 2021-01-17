f43079cd9eaa6f33bc5efd6077f75c3aecad5bb6
aaronkh <huang.aaron03@gmail.com>
2021-01-16 16:10:17 -0800

    finish vscode front

--- ext/Provider.js
+++ ext/Provider.js
@@ -1,13 +1,11 @@
-const uuid = require('uuid').v4
 const vscode = require('vscode')
-const Game = require('./templates/Game')
-const Splash = require('./templates/Splash')
+const path = require('path')
 const Renderer = require('./templates/Renderer')
 
 const PAGES = {
-    splash: Splash,
-    game: Game,
-    // end: End
+    splash: require('./templates/End'),
+    game: require('./templates/Game'),
+    end: require('./templates/End')
 }
 
 class Provider {
@@ -34,6 +32,7 @@ class Provider {
         this.panel = panel
 
         panel.webview.html = Renderer
+        panel.webview.onDidReceiveMessage(m => this._onMessage(m))
         if (this.gameId) {
             // Game has already started  
             this._loadHTML(
@@ -56,12 +55,89 @@ class Provider {
         this.panel = null
     }
 
+    _onMessage(m) {
+        if (!m || !m.type) console.error('Message missing type', m)
+        console.log('Message received', m)
+        let uri = null
+        switch (m.type) {
+            case 'play-again':
+                this.player = {
+                    name: '',
+                    id: ''
+                }
+                this._loadHTML(PAGES['splash'])
+                break
+            case 'join':
+                // TODO: check with server to get valid join code
+                // Expects a player id and game id from server
+                // Go next
+                this.player = {
+                    name: m.name,
+                    id: 'testid-123',
+                }
+                this.gameId = 'testgame-123'
+                this._loadHTML(
+                    PAGES['game'],
+                    this.player
+                )
+                break
+            case 'ready':
+                // TODO: send ready signal to server
+                break
+            case 'unready':
+                break
+            case 'end':
+                // Redirect to end screen
+                break
+            case 'sign-out':
+                this.player = {
+                    name: '',
+                    id: ''
+                }
+                // TODO: also clear from server
+                this._loadHTML(PAGES['splash'])
+                break
+            case 'time-reminder':
+                if (m.time > 60)
+                    vscode.window.showInformationMessage(`${Math.floor(m.time / 60)} minutes re.`)
+                else
+                    vscode.window.showInformationMessage(`${m.time} seconds remaining.`)
+                break
+            case 'game-start':
+                // Open new editor with template code
+                uri = path.join(__dirname, 'skeleton.html')
+                // TODO: get ending time from server, send game start signal
+                uri = vscode.Uri.file(uri)
+                vscode.workspace.openTextDocument(uri).then(doc => {
+                    vscode.window.showTextDocument(doc, {viewColumn: vscode.ViewColumn.Beside})
+                })
+                .then(editor => {
+                    this._updateTime(60 * 15, editor)
+                })
+                .catch(e => console.error('error', e))
+                break
+        }
+    }
+
+    _updateTime(startTime, editor) {
+        this._sendMessage({
+            type: 'time',
+            time: startTime
+        })
+        // TODO: send this to server
+        const text = editor.document.getText()
+
+        if(startTime < 500) return
+        window.setTimeout(() => {
+            this._updateTime(startTime - 1000)
+        }, 1000);
+    }
+
     _sendMessage(d) {
         // JS message passing: https://code.visualstudio.com/api/extension-guides/webview#scripts-and-message-passing
         if (!this.panel) {
             return
         }
-        console.log(d)
         this.panel.webview.postMessage(d)
     }
 
@@ -69,7 +145,8 @@ class Provider {
         if (!this.panel) return
         // Render each part of data then pass into 
         // message event
-        this._sendMessage({type: 'render', ...page.render({data})})
+        const payload = page.render(data)
+        this._sendMessage({ type: 'render', ...payload })
     }
 }
 
--- ext/templates/Game.js
+++ ext/templates/Game.js
@@ -1,114 +1,138 @@
 const Template = require("./Template");
 
-const Game = new Template(`
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <style type="text/css">
-        * {
-            transition: 0.2s
-        }
-
-        body,
-        html {
-            background: black;
-            color: white;
-            font-family: 'Poppins', sans-serif;
-            height: 100%;
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            min-width: 100px;
-        }
-
-        #main {
-            margin: 0.5rem;
-            margin-left: 1rem;
-            margin-right: 1rem;
-        }
-
-        a {
-            color: inherit;
-        }
-
-        .invisible {
-            display: none;
-        }
-
-        .button {
-            background: #49b738;
-            text-align: center;
-            margin-top: 6px;
-            cursor: pointer;
-            padding: 1rem;
-            margin-bottom: 4px;
-            width: 200px
-        }
-
-        .button-red {
-            background: #dc004e;
-        }
-
-        #content {
-            /* reset body styles */
-            background: white;
-            color: black;
-            font-family: serif;
-            margin: 0;
-            padding: 0;
-        }
-    </style>
-</head>
-
-<body>
-    <div id="main">
+const html = `
+<div id="main">
+    <div id="waiting" class="invisibule">
         <h1 id="please-wait"> Please wait... </h1>
-        <h1 id="time" class="invisible"> {{4:58}}</h1>
         <p> 
             We'll begin as soon as everyone's ready.
         <p>
         <div class="button">Ready</div>
         <p> 
-            Editing as <b>{{name}}</b>. Not you? 
-            <a href="http://localhost:{{port}}/sign-out">Sign out.</a> 
+        Editing as <b>{{name}}</b>. Not you? 
+        <!-- TODO: make this button also remove from server -->
+        <a id="sign-out">Sign out.</a> 
         </p>
     </div>
+    <div id="game" class="invisible">
+        <h1 id="time">14:30 remaining</h1>
+        <p>Try to copy the site below <b>without</b> running your code. </p>
+        <p>Tip: click on images to copy their <code>src</code> to your clipboard.</p>
+    </div>
+    </div>
     <div id="content"></div>
-    <script>
-            // Ready button signal 
-            // Show progress bar?
-            // On Game end
-            let isReady = false
-            const rdyButton = document.querySelector('.button')
+`
+const css = `
+    * {
+    }
+    code {
+        font-size: 1.2rem
+    }
+    .body,
+    html {
+        background: black;
+        color: white;
+        font-family: 'Poppins', sans-serif;
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        min-width: 100px;
+    }
 
-            rdyButton.addEventListener('click', async () => {
-                if(!isReady) {
-                    // TODO: Do websockets logic
-                    rdyButton.classList.add('button-red')
-                    rdyButton.innerText = 'Unready'
-                } else {
-                    // TODO: Do websockets logic
-                    rdyButton.classList.remove('button-red')
-                    rdyButton.innerText = 'Ready'
-                }
-                isReady = !isReady
-            })
+    #main {
+        margin: 0.5rem;
+        margin-left: 1.5rem;
+        margin-right: 1.5rem;
+    }
 
-            const content = document.querySelector('#id')
-            function start() {
-                // Start timer
-                // Load page
-            }
-    </script>
-</body>
+    a {
+        color: inherit;
+        text-decoration: underline;
+    }
 
-</html>
-`)
+    .invisible {
+        display: none;
+    }
+
+    .button {
+        background: #49b738;
+        text-align: center;
+        margin-top: 6px;
+        cursor: pointer;
+        padding: 1rem;
+        margin-bottom: 4px;
+        width: 200px
+    }
+
+    .button-red {
+        background: #dc004e;
+    }
+
+    #content {
+        /* reset body styles */
+        background: white;
+        color: black;
+        font-family: serif;
+        margin: 0;
+        padding: 0;
+    }
+`
+const js = `
+let isReady = false
+const rdyButton = document.querySelector('.button')
+const content = document.querySelector('#content')
+
+document.getElementById('sign-out').addEventListener('click', () => vscode.postMessage({type: 'sign-out'}))
+
+rdyButton.addEventListener('click', async () => {
+    if(!isReady) { // Send ready signal
+        vscode.postMessage({type: 'ready'})
+        rdyButton.classList.add('button-red')
+        rdyButton.innerText = 'Unready'
+    } else { // Send unready signal
+        vscode.postMessage({type: 'unready'})
+        rdyButton.classList.remove('button-red')
+        rdyButton.innerText = 'Ready'
+    }
+    isReady = !isReady
+})
+
+window.addEventListener('message', (m) => {
+    if(m.type === 'time') updateTimer(m.time)
+})
+window.addEventListener('message', m => {
+    if(m.type === 'content') loadContent(m.content)
+})
+
+function loadContent(c) {
+    content.innerHTML = c
+    const images = content.querySelectorAll('img')
+    images.forEach(img => {
+        img.addEventListener('click', () => navigator.clipboard.writeText(img.src))
+    })
+}
+
+function updateTimer(time) {
+    // Send reminders
+    const reminderTimes = []
+    if(reminderTimes.includes(time))
+        vscode.postMessage({
+            type: 'time-reminder',
+            time
+        })
+
+    // Update HTML
+    const s = time%60
+    const m = Math.floor(time/60)
+    let t = m+":"
+    if(s===0) t += '00'
+    else if(s<10) t+='0'+s
+    else t+=s
+    document.getElementById('time').innerText = t + ' remaining'
+}
+`
+
+const Game = new Template(html, css, js)
 
 module.exports = Game
